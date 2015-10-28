39fe5f31aba562165ef18c4b3319455dec1a9fe7
Antonio Carlos Mendonça Filho <antoniocarlosmendoncafilho@gmail.com>
2015-10-28 16:46:59 -0200

    Página de produtos adicionada e mais 2 gráficos.

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,6 @@ router.get('/', function(req, res, next) {
 		connection.query('select sum(valor_total) as soma, DAYNAME(dt_venda) as dia from vendas group by DAYNAME(dt_venda) order by sum(valor_total) desc;', function(err, rows, fields) {
 		  if (err) throw err;
 		  else {
-			  //console.log('The solution is: ', rows );
 			  var jsonObj = [];
 			  for(var i in rows){
 				  item = {};
@@ -28,7 +27,6 @@ router.get('/', function(req, res, next) {
 				  jsonObj.push(item);
 			  }
 			  
-			  //res.render('index', { chartData: JSON.stringify(jsonObj) });		
 			//select pra ver as vendas por mes 
 				connection.query('select sum(valor_total) as soma, MONTHNAME(dt_venda) as mes from vendas group by MONTHNAME(dt_venda) order by MONTH(dt_venda) asc;', function(err, rows, fields) {
 					  if (err) throw err;
@@ -40,14 +38,10 @@ router.get('/', function(req, res, next) {
 							  item["valor_total"] = rows[i].soma;
 							  item["mes"] = rows[i].mes;
 							  jsonObj2.push(item);
-						  }
-						  
+						  }  
 						  res.render('index', { chartData: JSON.stringify(jsonObj) , chartData2: JSON.stringify(jsonObj2) });		
 					  }
-
 					});
-				
-				
 					connection.end();[]
 					
 		  }
@@ -56,6 +50,55 @@ router.get('/', function(req, res, next) {
 		
   
 	
+});
+
+
+router.get('/produto.jade', function(req, res, next) {
+		var mysql      = require('mysql');
+		var connection = mysql.createConnection({
+			  host     : 'localhost',
+			  user     : 'root',
+			  password : 'root',
+			  database : 'sistema'
+			});
+
+			connection.connect();
+			
+			
+			//select pra vendas por dia da semana 
+			connection.query('select linha_produto as linha, SUM(qte_produto*valor_produto) as soma from produto inner join detalhes_venda on produto.id_produto = detalhes_venda.produto_id_produto group by linha_produto order by SUM(qte_produto*valor_produto) asc;', function(err, rows, fields) {
+			  if (err) throw err;
+			  else {
+				  var jsonObj = [];
+				  for(var i in rows){
+					  item = {};
+					  item["valor_total"] = rows[i].soma;
+					  item["dia"] = rows[i].linha;
+					  jsonObj.push(item);
+				  }
+				  
+				//select pra ver as vendas por mes 
+					connection.query('select area_produto as area, SUM(qte_produto*valor_produto) as soma from produto inner join detalhes_venda on produto.id_produto = detalhes_venda.produto_id_produto group by area_produto order by sum(qte_produto*valor_produto) asc;', function(err, rows, fields) {
+						  if (err) throw err;
+						  else {
+							  //console.log('The solution is: ', rows );
+							  var jsonObj2 = [];
+							  for(var i in rows){
+								  item = {};
+								  item["valor_total"] = rows[i].soma;
+								  item["mes"] = rows[i].area;
+								  jsonObj2.push(item);
+							  }  
+							  res.render('produto.jade', { chartData: JSON.stringify(jsonObj) , chartData2: JSON.stringify(jsonObj2) });		
+						  }
+						});
+						connection.end();[]
+						
+			  }
+
+			});
+			
+			
 });
 
 module.exports = router;
